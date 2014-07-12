308919297aca1ee81c3c3e64a9282441d38ffb85
Aleksandr Mazurenko <Aleksandr_Mazurenko@epam.com>
2014-07-12 09:05:00 +0300

    init commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'self-study'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures self-study'
+long_description 'Installs/Configures self-study'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: self-study
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
